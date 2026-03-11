fdcaf7c45470635b7594b30a29226f8b83a5b667
allemkarthik <allemkarthik@gmail.com>
2026-03-11 12:40:10 -0700

    optimized approach using recursion

--- LinkedList/swapNodesInpair.js
+++ LinkedList/swapNodesInpair.js
@@ -27,3 +27,16 @@ var swapnodes=function(head){
     }
     return dummy.next
 }
+
+
+// optimize approach using recursion
+var swapPairs=function(head){
+    if(!head || !head.next) return head
+
+    let l=head;
+    let r=head.next;
+    
+    l.next=swapPairs(r.next);
+    r.next=l
+    return r
+}
