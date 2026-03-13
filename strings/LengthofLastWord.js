7c77ee05040cabc3dd11fabe011f95282a4727e9
allemkarthik <allemkarthik@gmail.com>
2026-03-12 20:40:36 -0700

    optimized approach

--- strings/LengthofLastWord.js
+++ strings/LengthofLastWord.js
@@ -48,3 +48,18 @@ var lengthofLastWord=function(s){
     return count;
 }
 
+// approach 2 using one loop
+var lengthOfLastWord = function(s) {
+    let n=s.length-1;
+    let count=0;
+    while(n>=0){
+        if(s[n]!=" "){
+            count++
+        }else if(count>0){
+            break;
+        }
+        --n
+    }
+    return count
+
+};
