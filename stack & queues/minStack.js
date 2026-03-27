2a8dc9a3d9a54a682645910e87bd105682df04d1
allemkarthik <allemkarthik@gmail.com>
2026-03-27 12:28:11 -0700

    minStack with O(1)

---
+++ stack & queues/minStack.js
@@ -0,0 +1,43 @@
+// Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+// Implement the MinStack class:
+
+// MinStack() initializes the stack object.
+// void push(int val) pushes the element val onto the stack.
+// void pop() removes the element on the top of the stack.
+// int top() gets the top element of the stack.
+// int getMin() retrieves the minimum element in the stack.
+// You must implement a solution with O(1) time complexity for each function.
+
+ 
+
+// Example 1:
+
+// Input
+// ["MinStack","push","push","push","getMin","pop","top","getMin"]
+// [[],[-2],[0],[-3],[],[],[],[]]
+
+// Output
+// // [null,null,null,null,-3,null,0,-2]
+
+var minStack=function(){
+    this.s=[]
+}
+minStack.prototype.push=function(val){
+    if(this.s.length==0){
+        this.s.push([val,val])
+    }else{
+        prevmin=this.s[this.s.length-1][1]
+        minval=Math.min(val,prevmin)
+        this.s.push(val,minval)
+    }
+}
+minStack.prototype.pop=function(){
+    this.s.pop()
+}
+minStack.prototype.top=function(){
+    return this.s[this.s.length-1][0]
+}
+minStack.prototype.minval=function(){
+    return this.s[this.s.length-1][1]
+}
