4b61bad6a4bf3e9a1e2a74b93dfd90f1ba6f8680
allemkarthik <allemkarthik@gmail.com>
2026-03-28 23:25:34 -0700

    remove outer paranthesis 1021

---
+++ stack & queues/removeOuterParathensis_1021.js
@@ -0,0 +1,54 @@
+// A valid parentheses string is either empty "", "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.
+
+// For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+// A valid parentheses string s is primitive if it is nonempty, and there does not exist a way to split it into s = A + B, with A and B nonempty valid parentheses strings.
+
+// Given a valid parentheses string s, consider its primitive decomposition: s = P1 + P2 + ... + Pk, where Pi are primitive valid parentheses strings.
+
+// Return s after removing the outermost parentheses of every primitive string in the primitive decomposition of s.
+
+ 
+
+// Example 1:
+
+// Input: s = "(()())(())"
+// Output: "()()()"
+// Explanation: 
+// The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+// After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+// Example 2:
+
+// Input: s = "(()())(())(()(()))"
+// Output: "()()()()(())"
+// Explanation: 
+// The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+// After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
+// Example 3:
+
+// Input: s = "()()"
+// Output: ""
+// Explanation: 
+// The input string is "()()", with primitive decomposition "()" + "()".
+// After removing outer pa
+
+
+var outerparanthesis=function(s){
+    let stack=[]
+    let res=""
+    for(let i=0;i<s.length;i++){
+        if(s[i]=="("){
+            stack.push(s[i])
+            let len=stack.length
+            if(len>1){
+                res=res+s[i]
+            }
+        }else{
+            let len=stack.length
+            if(len>1){
+                res=res+s[i]
+            }
+            stack.pop()
+        }
+    }
+    return res;
+}
