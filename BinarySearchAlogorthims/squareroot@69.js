c4d8a19da717b2ec003ad7aeb1240f68570d10ae
allemkarthik <allemkarthik@gmail.com>
2026-04-06 13:11:09 -0700

    squareRoot @69

---
+++ BinarySearchAlogorthims/squareroot@69.js
@@ -0,0 +1,38 @@
+// Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+// You must not use any built-in exponent function or operator.
+
+// For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+ 
+
+// Example 1:
+
+// Input: x = 4
+// Output: 2
+// Explanation: The square root of 4 is 2, so we return 2.
+// Example 2:
+
+// Input: x = 8
+// Output: 2
+// Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+
+var mySqrt = function(x) {
+    // corner case
+    if(x<2) return x;
+
+    let left=2
+    let right=Math.floor(x/2);
+    while(left<=right){
+        let m=Math.floor((left+right)/2)
+        if(x==m**2){
+            return m;
+        }else if(x<m**2){
+            right=m-1
+        }
+        else{
+            left=m+1
+        }
+    }
+    return right
+
+};
